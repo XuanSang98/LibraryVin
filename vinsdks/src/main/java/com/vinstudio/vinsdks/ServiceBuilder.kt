474dc7642b1d5da0fc8a1b4ee1fb71bf4e9c0899
XuanSang98 <sangnx@vinsofts.net>
2020-08-18 15:16:50 +0700

    Library Vin

--- vinsdks/src/main/java/com/vinstudio/vinsdks/ServiceBuilder.kt
+++ vinsdks/src/main/java/com/vinstudio/vinsdks/ServiceBuilder.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
-    val BASE_URL = "https://vin-crypto-market.herokuapp.com/api"
+    val BASE_URL = "https://vin-crypto-market.herokuapp.com/api/"
     fun <T> buildService(service: Class<T>): T = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
